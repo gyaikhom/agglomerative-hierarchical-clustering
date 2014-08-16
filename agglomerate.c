4b0e683e127d8b310b15ed4c4aa70fd93751174f
Gagarine Yaikhom <gyaikhom@gmail.com>
2014-08-16 18:18:04 +0100

    Cluster initialisation now works.

--- agglomerate.c
+++ agglomerate.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,11 +6,13 @@
 typedef struct cluster_s cluster_t;
 typedef struct cluster_node_s cluster_node_t;
 typedef struct proximity_node_s proximity_node_t;
+typedef struct item_s item_t;
 
 struct cluster_s {
      int num_items;
      int num_clusters;
      cluster_node_t *nodes;
+     float **distances;
 };
 
 struct cluster_node_s {
@@ -29,18 +32,23 @@ struct proximity_node_s {
      proximity_node_t *next, *prev;
 };
 
+struct item_s {
+     float x, y;
+     char *label;
+};
+
 cluster_node_t *add_leaf(cluster_t *cluster, const char *label) {
-     cluster_node_t *t = &cluster->nodes[cluster->num_items];
-     if ((t->label = (char *)
-          calloc(strlen(label), sizeof(char))) == NULL) {
-          fprintf(stderr, "Failed to allocate label.\n");
-          t = NULL;
-     } else {
+     int retval = 0;
+     cluster_node_t *t = &(cluster->nodes[cluster->num_clusters]);
+     t->label = (char *) calloc(strlen(label), sizeof(char));
+     if (t->label) {
           strlcpy(t->label, label, sizeof(label));
           t->is_used = 1;
           t->is_root = 1;
-          cluster->num_items++;
           cluster->num_clusters++;
+     } else {
+          fprintf(stderr, "Failed to allocate label.\n");
+          t = NULL;
      }
      return t;
 }
@@ -57,25 +65,31 @@ void free_proximity_list(proximity_node_t *node) {
 void free_cluster(cluster_t * cluster) {
      int i;
      cluster_node_t node;
-     for (i = 0; i < cluster->num_items; ++i) {
-          node = cluster->nodes[i];
-          if (node.label != NULL)
-               free(node.label);
-          if (node.proximity != NULL)
-               free_proximity_list(node.proximity);
+     if (cluster) {
+          if (cluster->nodes) {
+               for (i = 0; i < cluster->num_items; ++i) {
+                    node = cluster->nodes[i];
+                    if (node.label)
+                         free(node.label);
+                    if (node.proximity)
+                         free_proximity_list(node.proximity);
+               }
+               free(cluster->nodes);
+          }
+          if (cluster->distances)
+               free(cluster->distances);
+          free(cluster);
      }
-     free(cluster);
 }
 
 void insert_before(proximity_node_t *current, proximity_node_t *proximity,
                    cluster_node_t *node) {
      proximity->next = current;
-     if (current->prev == NULL) {
-          node->proximity = proximity;
-     } else {
+     if (current->prev) {
           current->prev->next = proximity;
           proximity->prev = current->prev;
-     }
+     } else
+          node->proximity = proximity;
      current->prev = proximity;
 }
 
@@ -86,7 +100,7 @@ void insert_after(proximity_node_t *current, proximity_node_t *proximity) {
 
 void insert_sorted(cluster_node_t *node, proximity_node_t *proximity) {
      proximity_node_t *temp = node->proximity;
-     while(temp->next != NULL) {
+     while (temp->next) {
           if (temp->distance >= proximity->distance) {
                insert_before(temp, proximity, node);
                return;
@@ -99,25 +113,23 @@ void insert_sorted(cluster_node_t *node, proximity_node_t *proximity) {
           insert_after(temp, proximity);
 }
 
-proximity_node_t *add_proximity(cluster_node_t *node, int index, int target,
-                                const float **distance) {
-     proximity_node_t *proximity;
-     if ((proximity = (proximity_node_t *)
-          calloc(1, sizeof(proximity_node_t))) == NULL) {
-          fprintf(stderr, "Failed to allocate proximity node.\n");
-     } else {
+proximity_node_t *add_proximity(cluster_t *cluster, int index, int target) {
+     proximity_node_t *proximity = (proximity_node_t *)
+          calloc(1, sizeof(proximity_node_t));
+     if (proximity) {
           proximity->target = target;
-          proximity->distance = distance[index][target];
-          if (node->proximity == NULL)
-               node->proximity = proximity;
-          else
+          proximity->distance = cluster->distances[index][target];
+          cluster_node_t *node = &(cluster->nodes[index]);
+          if (node->proximity)
                insert_sorted(node, proximity);
-     }
+          else
+               node->proximity = proximity;
+     } else 
+          fprintf(stderr, "Failed to allocate proximity node.\n");
      return proximity;
 }
 
-cluster_t *update_proximity(cluster_t *cluster, int index,
-                            const float **distance) {
+cluster_t *update_proximity(cluster_t *cluster, int index) {
      int i = 1, target = index;
      cluster_node_t node = cluster->nodes[index], temp;
      if (node.is_used) {
@@ -129,7 +141,7 @@ cluster_t *update_proximity(cluster_t *cluster, int index,
                     break;
                }
                if (temp.is_root)
-                    add_proximity(&node, index, target, distance);
+                    add_proximity(cluster, index, target);
           }
      } else {
           fprintf(stderr, "Supplied node with index %d is invalid\n", index);
@@ -138,46 +150,120 @@ cluster_t *update_proximity(cluster_t *cluster, int index,
      return cluster;
 }
 
-cluster_t *add_leaves(cluster_t *cluster, const char *labels[],
-                      const float **distance) {
+float euclidean_distance(item_t a, item_t b) {
+     return sqrt(pow((a.x - b.x), 2) + pow((a.y - b.y), 2));
+}
+
+void fill_eucliden_distances(float **matrix, int num_items,
+                             const item_t items[]) {
+     int i, j;
+     for (i = 0; i < num_items; ++i)
+          for (j = 0; j < num_items; ++j) {
+               matrix[i][j] = euclidean_distance(items[i], items[j]);
+               matrix[j][i] = matrix[i][j];
+          }
+}
+
+float **generate_distance_matrix(int num_items, const item_t items[]) {
+     int i, j;
+     float **matrix = matrix = (float **) calloc(num_items, sizeof(float *));
+     if (matrix) {
+          for (i = 0; i < num_items; ++i) {
+               matrix[i] = (float *) calloc(num_items, sizeof(float));
+               if (!matrix[i]) {
+                    fprintf(stderr, "Failed to allocate distance matrix.\n");
+                    num_items = i;
+                    for (i = 0; i < num_items; ++i)
+                         free(matrix[i]);
+                    free(matrix);
+                    matrix = NULL;
+                    break;
+               }    
+          }
+          if (matrix)
+               fill_eucliden_distances(matrix, num_items, items);
+     } else
+          fprintf(stderr, "Failed to allocate distance matrix.\n");
+     return matrix;
+}
+
+cluster_t *add_leaves(cluster_t *cluster, const item_t items[]) {
      int i = 0;
      cluster_node_t *node;
      for (i = 0; i < cluster->num_items; ++i) {
-          node = add_leaf(cluster, labels[i]);
-          if (node == NULL) {
-               free_cluster(cluster);
+          if (add_leaf(cluster, items[i].label))
+               update_proximity(cluster, i);
+          else {
                cluster = NULL;
                break;
-          } else
-               update_proximity(cluster, i, distance);
-     }
-     return cluster;
-}
-
-cluster_t *merge_clusters(cluster_t *cluster) {
-     return cluster;
-}
-
-cluster_t *agglomerate(int num_items, const char *labels[], const float **distance) {
-     cluster_t *cluster;
-     if ((cluster = (cluster_t *) calloc(1, sizeof(cluster_t))) == NULL)
-          fprintf(stderr, "Failed to allocate cluster.\n");
-     else {
-          cluster->num_items = num_items;
-          cluster->num_clusters = 0;
-          if ((cluster->nodes = (cluster_node_t *)
-               calloc(2 * num_items - 1, sizeof(cluster_node_t))) == NULL) {
-               fprintf(stderr, "Failed to allocate cluster nodes.\n");
-               free(cluster);
-               cluster = NULL;
-          } else {
-               add_leaves(cluster, labels, distance);
-               merge_clusters(cluster);
           }
      }
      return cluster;
 }
 
+cluster_t *merge_clusters(cluster_t *cluster) {
+     
+     return cluster;
+}
+
+cluster_t *agglomerate(int num_items, const item_t items[]) {
+     cluster_t *cluster = (cluster_t *) calloc(1, sizeof(cluster_t));
+     if (cluster) {
+          cluster->nodes = (cluster_node_t *)
+               calloc(2 * num_items - 1, sizeof(cluster_node_t));
+          if (cluster->nodes) {
+               cluster->distances = generate_distance_matrix(num_items, items);
+               if (!cluster->distances)
+                    goto cleanup;
+               cluster->num_items = num_items;
+               cluster->num_clusters = 0;
+               if (add_leaves(cluster, items))
+                    merge_clusters(cluster);
+               else
+                    goto cleanup;
+          } else {
+               fprintf(stderr, "Failed to allocate cluster nodes.\n");
+               goto cleanup;
+          }
+     } else
+          fprintf(stderr, "Failed to allocate cluster.\n");
+     goto done;
+
+cleanup:
+     free_cluster(cluster);
+     cluster = NULL;
+
+done:
+     return cluster;
+}
+
+void print_cluster_node(cluster_node_t *node) {
+     proximity_node_t *t;
+     if (node->label)
+          fprintf(stdout, "%s\n", node->label);
+     t = node->proximity;
+     while (t) {
+          fprintf(stdout, "(%d, %f), ", t->target, t->distance);
+          t = t->next;
+     }
+     fprintf(stdout, "\n");
+}
+
+void print_cluster(cluster_t *cluster) {
+     int i;
+     for (i = 0; i < cluster->num_clusters; ++i)
+          print_cluster_node(&cluster->nodes[i]);
+}
+
 int main(int argc, char **argv) {
+     item_t items[] = {
+          { .label = "A", .x = 0.5, .y = 0.5 },
+          { .label = "B", .x = 5.5, .y = 0.5 },
+          { .label = "C", .x = 5.5, .y = 5.5 },
+          { .label = "D", .x = 0.5, .y = 5.5 }
+     };
+     cluster_t *cluster = agglomerate(sizeof(items) / sizeof(items[0]), items);
+     print_cluster(cluster);
+     free_cluster(cluster);
      return 0;
 }
